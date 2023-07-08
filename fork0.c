36fb8451073c6a4deacd9640b72549f9cb11e145
ruchen <ruchen.gs@alibaba-inc.com>
2023-07-08 15:21:18 +0800

    fork0

--- fork0.c
+++ fork0.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<unistd.h>
 
+// 父进程死亡，子进程被init进程收养
 int main(){
     pid_t pid;
     int x = 1;
@@ -10,10 +11,11 @@ int main(){
 
     // 子进程
     if(pid == 0){
-        sleep(5);
+        sleep(10);
         printf("child: x=%d\n", ++x);
         exit(0);
     }
+    sleep(5);
     // 主进程
     printf("parent: x=%d\n", --x);
     exit(0);
